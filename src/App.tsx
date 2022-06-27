3f631caa861763488d1f059159ca205b42fae871
Erik Ostlund <eostlund74@gmail.com>
2022-06-27 11:08:42 -0500

    Installed Styled Components
    And set-up some basic components with some styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+import {
+	AppContainer,
+	ColumnContainer,
+	ColumnTitle,
+	CardContainer
+} from './styles';
+
 export const App = () => {
-	return null;
+	return (
+		<AppContainer>
+			<ColumnContainer>
+				<ColumnTitle>Todo</ColumnTitle>
+				<CardContainer>First Item</CardContainer>
+				<CardContainer>Second Item</CardContainer>
+				<CardContainer>Third Item</CardContainer>
+			</ColumnContainer>
+		</AppContainer>
+	);
 };
