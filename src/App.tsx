024a1fe3b872ab10cf378bdd773b85cbb35c07ae
Erik Ostlund <eostlund74@gmail.com>
2022-06-29 17:21:06 -0500

    Created Global State
    Using useContext and a provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { AppContainer } from './styles';
 import { Column } from './components/Column';
 import { AddNewItem } from './components/AddNewItem';
+import { useAppState } from './state/AppStateContext';
 
 export const App = () => {
+	const { lists } = useAppState();
+
 	return (
 		<AppContainer>
-			<Column text='Todo' />
+			{lists.map((list) => (
+				<Column key={list.id} id={list.id} text={list.text} />
+			))}
+
 			<AddNewItem toggleButtonText='+ Add New List' onAdd={console.log} />
 		</AppContainer>
 	);
--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,18 +1,26 @@
 import { ColumnContainer, ColumnTitle } from '../styles';
 import { AddNewItem } from './AddNewItem';
 import { Card } from './Card';
+import { useAppState } from '../state/AppStateContext';
 
 type ColumnProp = {
+	id: string;
 	text: string;
 };
 
-export const Column = ({ text }: ColumnProp) => {
+export const Column = ({ text, id }: ColumnProp) => {
+	const { getTasksByListId } = useAppState();
+
+	const tasks = getTasksByListId(id);
+
 	return (
 		<ColumnContainer>
 			<ColumnTitle>{text}</ColumnTitle>
-			<Card text='App Architecture' />
-			<Card text='Learn Typescript' />
-			<Card text='Static Typing' />
+
+			{tasks.map((task) => (
+				<Card key={task.id} id={task.id} text={task.text} />
+			))}
+
 			<AddNewItem toggleButtonText='+ Add New Card' onAdd={console.log} dark />
 		</ColumnContainer>
 	);
