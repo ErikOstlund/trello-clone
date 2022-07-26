87708e1e51a1bee128d4eb29f9249b360223b8da
Erik Ostlund <eostlund74@gmail.com>
2022-07-26 11:36:44 -0500

    MOVE_TASK case in reducer

--- src/state/appStateReducer.ts
+++ src/state/appStateReducer.ts
@@ -51,8 +51,35 @@ export const appStateReducer = (
 			draft.lists = moveItem(draft.lists, dragIndex, hoverIndex);
 			break;
 		}
+		case "MOVE_TASK": {
+			const { draggedItemId, hoverItemId, sourceColumnId, targetColumnId } =
+				action.payload;
+
+			const sourceListIndex = findItemIndexById(draft.lists, sourceColumnId);
+			const targetListIndex = findItemIndexById(draft.lists, targetColumnId);
+
+			const dragIndex = findItemIndexById(
+				draft.lists[sourceListIndex].tasks,
+				draggedItemId
+			);
+			const hoverIndex = hoverItemId
+				? findItemIndexById(draft.lists[targetListIndex].tasks, draggedItemId)
+				: 0;
+
+			const item = draft.lists[sourceListIndex].tasks[dragIndex];
+
+			// Remove the task from the source list
+			draft.lists[sourceListIndex].tasks.splice(dragIndex, 1);
+
+			// Add the task to the target list
+			draft.lists[targetListIndex].tasks.splice(hoverIndex, 0, item);
+			break;
+		}
 		case "SET_DRAGGED_ITEM": {
 			draft.draggedItem = action.payload;
+			break;
+		}
+		default: {
 			break;
 		}
 	}
